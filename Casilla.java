91857dd16652a79f74f33176cc8c49646283f3e1
apt-getSanchez <sanchez1997_zgz@hotmail.com>
2017-05-25 08:08:31 +0200

    Ejercicio de 2048 mejorado

--- Casilla.java
+++ Casilla.java
@@ -1,39 +1,43 @@
 import java.awt.*;
 import java.applet.*;
 
-public class Casilla extends Rectangle{
-	public static final int DIM = 100;
-	private Image imagen;
-	Image reverso;
-	private boolean descubierta;
-    public Casilla(int posx, int posy, Image img, Image rev) {
-		super(posx, posy, DIM, DIM);
-    	imagen = img;
-    	reverso = rev;
-    	descubierta = false;
+public class Casilla {
+	Color coloresFondo[] = {Color.pink, Color.orange, Color.blue, Color.green, Color.black, Color.magenta, Color.cyan, Color.magenta, Color.red, Color.lightGray, Color.yellow};
+	Color coloresLetra[] = {Color.white, Color.black, Color.white, Color.white, Color.white, Color.white, Color.white, Color.white, Color.white, Color.black, Color.black};
+	int x, y;
+	public static final int TAM = 50;
+	private int valor;
+    public Casilla(int posx, int posy) {
+    	x = posx; 
+    	y = posy;
+		valor = 0;
     }
 
-	public Image getImagen(){
-		return imagen;
-	}
-	
-	public void setImagen(Image nueva){
-		imagen = nueva;
-	}
-	
-	public boolean getDescubierta(){
-		return descubierta;
+	public int getValor(){
+		return valor;
 	}
 
-	public void setDescubierta(boolean d){
-		descubierta = d;
+	public void setValor(int num){
+		valor = num;
 	}
-
     public void dibujar(Graphics g, Applet a){
-		if(descubierta)
-			g.drawImage(imagen, x, y, width, height, a);
-		else
-			g.drawImage(reverso, x, y, width, height, a);
+    	g.setColor(Color.gray);
+		g.fillRect(x, y, TAM, TAM);
+		g.setColor(coloresFondo[obtenerIndice()]);
+		g.fillRect(x+2, y+2, TAM-4, TAM-4);
+		g.setFont(new Font("Arial", Font.BOLD, 20));
+		g.setColor(coloresLetra[obtenerIndice()]);
+		g.drawString((valor==0)?"":"" + valor, x+8, y+15);
+    }
+    
+    public int obtenerIndice(){
+    	int cuanto = 0; 
+    	int valorTemp = valor;
+    	while(!((valorTemp == 0) || (valorTemp == 1))){
+    		cuanto++;
+    		valorTemp = valorTemp / 2;
+    	}
+    	return cuanto;
     }
     
 }
