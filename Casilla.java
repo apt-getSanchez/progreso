fdaeca9544c50133b6e42fa58196cb700c4c852a
apt-getSanchez <sanchez1997_zgz@hotmail.com>
2017-05-25 08:08:04 +0200

    Ejercicio de encontrar la pareja de una imagen

--- Casilla.java
+++ Casilla.java
@@ -3,12 +3,31 @@ import java.applet.*;
 
 public class Casilla extends Rectangle{
 	public static final int DIM = 100;
-	Color letras[]={Color.black,Color.white};
-	Color fondos[]=;
 	private Image imagen;
-    public Casilla() {
-    
+	Image reverso;
+	private boolean descubierta;
+    public Casilla(int posx, int posy, Image img, Image rev) {
+		super(posx, posy, DIM, DIM);
+    	imagen = img;
+    	reverso = rev;
+    	descubierta = false;
     }
+
+	public Image getImagen(){
+		return imagen;
+	}
+	
+	public void setImagen(Image nueva){
+		imagen = nueva;
+	}
+	
+	public boolean getDescubierta(){
+		return descubierta;
+	}
+
+	public void setDescubierta(boolean d){
+		descubierta = d;
+	}
 
     public void dibujar(Graphics g, Applet a){
 		if(descubierta)
